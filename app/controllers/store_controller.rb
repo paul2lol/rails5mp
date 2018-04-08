2e0dd9143b193b16a7546b2eae9217f2582faeaf
Paul S <souradeep.paul@gmail.com>
2018-04-08 23:09:20 +0530

    'DoneWithLinking!'

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,9 +1,11 @@
 class StoreController < ApplicationController
   def index
+  	@lineitemcounter = LineItem.all.count
     @products = Product.order(:title)
     @counter = session[:counter]
     @counter.nil? ? @counter = 1 : @counter+=1
     session[:counter] = @counter
     @shown_message = "You have visted #@counter times" if @counter>2
+    @cart_message = "< View Cart (#@lineitemcounter)"
   end
 end
