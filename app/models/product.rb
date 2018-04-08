94222b5c659892646f7b52569e477d9e61d38208
Paul S <souradeep.paul@gmail.com>
2018-04-08 17:09:18 +0530

    'CartComplete!'

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,16 @@ class Product < ApplicationRecord
 	validates :title, length: {
 		minimum: 10
 	}
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+#...
+private
+# ensure that there are no line items referencing this product
+def ensure_not_referenced_by_any_line_item
+	unless line_items.empty?
+		errors.add(:base, 'Line Items present')
+		throw :abort
+	end
+end
 end
